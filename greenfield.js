d14a7c29fc4b937a5027ac6eec7f272f8e5fe9b6
Ghasem Kiani <ghasemkiani@gmail.com>
2023-10-18 11:19:12 +0330

    update

---
+++ greenfield.js
@@ -0,0 +1,25 @@
+import {Chain} from "./chain.js";
+
+const greenfield = Chain.add({
+	id: 1017,
+	tok: "BNB",
+	symbol: "gnfd",
+	name: "BNB Greenfield",
+	defaultUrl: "https://greenfield-chain.bnbchain.org:443/",
+	scanUrl: "https://greenfieldscan.com/api",
+	contracts: {
+		"WBNB": "",
+	},
+	contractProxies: {},
+});
+
+const greenfieldTestnet = Chain.add({
+	id: 5600,
+	tok: "BNB",
+	symbol: "gnfd-testnet",
+	name: "BNB Greenfield Testnet",
+	defaultUrl: "https://gnfd-testnet-fullnode-tendermint-us.bnbchain.org/",
+	scanUrl: "https://testnet.greenfieldscan.com/api",
+});
+
+export {greenfield, greenfieldTestnet};
