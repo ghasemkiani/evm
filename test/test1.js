3985a833b8780043f3d2a2f65fb7e16a66e7fcdd
Ghasem Kiani <ghasemkiani@gmail.com>
2024-08-31 19:42:29 +0330

    update

--- test/test1.js
+++ test/test1.js
@@ -1,13 +1,15 @@
-import test from "ava";
+import test from "node:test";
+import assert from "node:assert";
 
 import { Chain } from "../chain.js";
+import { ethereum } from "../ethereum.js";
 
 test("Chain.get", async (t) => {
   const chain = Chain.get("ethereum");
-  t.truthy(chain?.url);
+  assert.ok(chain?.url);
 });
 
 test("Chain.toGetInjected", async (t) => {
   const chain = await Chain.toGetInjected();
-  t.falsy(chain);
+  assert.ok(!chain);
 });
