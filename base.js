04e9e86ff1581eb9d63f32da21e4719f1ad38b94
Ghasem Kiani <ghasemkiani@gmail.com>
2025-03-18 16:30:01 +0330

    update

--- base.js
+++ base.js
@@ -9,6 +9,7 @@ const base = Chain.add({
   scanUrl: "https://api.basescan.org/api",
   contracts: {
     WETH: "0x4200000000000000000000000000000000000006",
+    cbBTC: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf",
     SHIB: "0xbfFb921F1a95727E5Db5c36bCb9080056f0C5761",
   },
   tokenDecimals: {},
