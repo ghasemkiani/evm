b3b4aa06a1202c6e2a0fc4a5bd9c0aec2f9332e3
Ghasem Kiani <ghasemkiani@gmail.com>
2025-06-11 16:01:35 +0330

    update

--- iwchainApp.js
+++ iwchainApp.js
@@ -6,15 +6,75 @@ import { chainer } from "./chainer.js";
 const iwchainApp = cutil.extend(chainer, {
   // network: null,
   defaultPrefsIWChainApp: {
-    //
+    gasLimitMax: null,
+    gasLimitK: null,
+    gasPriceK: null,
   },
   async toDefineInitOptionsIWChainApp() {
     let app = this;
+    app.commander.option(
+      "--set-gas-limit-max <gasLimitMax>",
+      "Gas limit max persistently",
+    );
+    app.commander.option(
+      "--set-gas-limit-k <gasLimitK>",
+      "Gas limit K persistently",
+    );
+    app.commander.option(
+      "--set-gas-price-k <gasPriceK>",
+      "Gas price K persistently",
+    );
+    app.commander.option("--gas-limit-max <gasLimitMax>", "Gas limit max");
+    app.commander.option("--gas-limit-k <gasLimitK>", "Gas limit K");
+    app.commander.option("--gas-price-k <gasPriceK>", "Gas price K");
   },
   async toApplyInitOptionsIWChainApp() {
     let app = this;
     let { network } = app;
     Chain.set(network);
+    
+    let { chain } = app;
+    let { prefs } = app;
+    let opts = app.commander.opts();
+    
+    if (cutil.a(prefs.gasLimitMax)) {
+      chain.gasLimitMax = cutil.asInteger(prefs.gasLimitMax);
+    }
+    if (cutil.a(prefs.gasLimitK)) {
+      chain.gasLimitK = cutil.asNumber(prefs.gasLimitK);
+    }
+    if (cutil.a(prefs.gasPriceK)) {
+      chain.gasPriceK = cutil.asNumber(prefs.gasPriceK);
+    }
+
+    if (cutil.a(opts.setGasLimitMax)) {
+      prefs.gasLimitMax = cutil.asInteger(opts.setGasLimitMax);
+      chain.gasLimitMax = cutil.asInteger(opts.setGasLimitMax);
+    }
+    if (cutil.a(opts.setGasLimitK)) {
+      prefs.gasLimitK = cutil.asNumber(opts.setGasLimitK);
+      chain.gasLimitK = cutil.asNumber(opts.setGasLimitK);
+    }
+    if (cutil.a(opts.setGasPriceK)) {
+      prefs.gasPriceK = cutil.asNumber(opts.setGasPriceK);
+      chain.gasPriceK = cutil.asNumber(opts.setGasPriceK);
+    }
+
+    if (cutil.a(opts.gasLimitMax)) {
+      chain.gasLimitMax = cutil.asInteger(opts.gasLimitMax);
+    }
+    if (cutil.a(opts.gasLimitK)) {
+      chain.gasLimitK = cutil.asNumber(opts.gasLimitK);
+    }
+    if (cutil.a(opts.gasPriceK)) {
+      chain.gasPriceK = cutil.asNumber(opts.gasPriceK);
+    }
+    
+    if (app.verbose) {
+      console.log(`chain.gasLimitMax: ${chain.gasLimitMax}`);
+      console.log(`chain.gasPriceK: ${chain.gasPriceK}`);
+      console.log(`chain.gasLimitK: ${chain.gasLimitK}`);
+    }
   },
 });
 
